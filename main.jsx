ba842cf836aaa3b5650891fa4633a6d4854f0366
zchar996 <zhutingyu96@gmail.com>
2026-06-21 21:53:05 +0800

    initial commit

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import RoundUp from "./RoundUp.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RoundUp />
-  </React.StrictMode>
-);
